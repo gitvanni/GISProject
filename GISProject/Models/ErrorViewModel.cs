98333af36424d8b91e6c9266e5a32044598d7309
gitvanni <giovanni.sdringola51@gmail.com>
2025-05-08 11:03:02 +0200

    Aggiungere i file di progetto.

---
+++ GISProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GISProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
